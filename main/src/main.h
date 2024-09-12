085d1e148a8d240aeae875bb5045c3c53ab3c028
dciuculin <92886957+dciuculin@users.noreply.github.com>
2024-09-12 11:26:58 -0400

    added more to main.h

--- main/src/main.h
+++ main/src/main.h
@@ -1,11 +1,15 @@
 
-class Student {
+#include<vector>
 
+class Student {
+    bool greediness;
+    int preferred_row;
 
 };
 
 class Classroom {
-
-
-
+    int seat_num;
+    int row_count;
+    int fullness;
+    std::vector<int> rows;
 };
