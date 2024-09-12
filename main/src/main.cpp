a29ad8214b33ac868118c4a829d3006d7e158b5f
robjob0704 <robertsjoshua@uufl.edu>
2024-09-12 12:06:09 -0400

    added basic classroom and student features and started function to initialize students

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,9 +1,45 @@
 #include "main.h"
 #include <iostream>
+using namespace std;
+
+
+vector<Student> initStudents(int num_students, int num_greedy){
+    vector<Student> students;
+    int greedy_added;
+
+    for(int i = 0; i < num_students; i++){
+
+        
+
+    }
+
+    return students;
+}
+
+void fillrows(int num_students){
+    // put a student into a row
+    
+
+
+    // call display function to portray all rows with new student
+}
+
 
 int main(){
 
-    std::cout << "game theory" << std::endl;
+    cout << "game theory" << std::endl;
+
+    int num_seats = 40;
+    int num_rows = 2;
+    int fullness = 50;
+    int num_greedy = 10;
+
+    Classroom Class(num_seats, num_rows, fullness); 
+
+    vector<Student> students = initStudents(num_seats / fullness , num_greedy);
+
+    // Class.rows = fillrows(num_seats / fullness); // fills all students and calls display function within
+
 
     return 0;
-}
+    }
--- main/src/main.h
+++ main/src/main.h
@@ -1,15 +1,23 @@
-
 #include<vector>
 
-class Student {
-    bool greediness;
+struct Student {
+
+    bool greedy;
     int preferred_row;
+    int ID;
+
+    Student(int greedy, int preferred_row, int ID) : greedy(greedy), preferred_row(preferred_row), ID(ID) {};
 
 };
 
-class Classroom {
+struct Classroom {
+
     int seat_num;
     int row_count;
     int fullness;
-    std::vector<int> rows;
+    std::vector<std::vector<int>> rows;
+    std::vector<Student> students;
+
+    Classroom(int seat_num, int row_count, int fullness) : seat_num(seat_num), row_count(row_count), fullness(fullness) {};
+
 };
